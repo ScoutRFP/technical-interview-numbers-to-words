824934f07ae2b7a694675945544054ad6d7e6d2a
Raimonds Samofals <samofals@gmail.com>
2016-01-04 13:11:55 +0200

    Initial commit

---
+++ spec/number_to_words_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../number_to_words'
+
+# NumberToWords.to_words(3)    # => "three"
+# NumberToWords.to_words(25)   # => "twenty five"
+# NumberToWords.to_words(2004) # => "two thousand four"
+
+describe NumberToWords do
+  pending "tests here"
+end
+
