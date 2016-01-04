5db7286888b4bc74672d6f7bad985416d78ca8ae
Edgars Jekabsons <jekabsons.edgars@gmail.com>
2016-01-04 15:25:04 +0200

    Adds gemset definition and read me

--- spec/number_to_words_spec.rb
+++ spec/number_to_words_spec.rb
@@ -1,9 +1,5 @@
 require_relative '../number_to_words'
 
-# NumberToWords.to_words(3)    # => "three"
-# NumberToWords.to_words(25)   # => "twenty five"
-# NumberToWords.to_words(2004) # => "two thousand four"
-
 describe NumberToWords do
   pending "tests here"
 end
